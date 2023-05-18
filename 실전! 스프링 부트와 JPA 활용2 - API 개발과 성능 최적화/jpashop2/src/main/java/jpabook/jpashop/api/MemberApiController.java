11ada0ddf27867cb66ab143255e9386ffd4bf477
Juyeon703 <sh6244@naver.com>
2023-05-18 20:58:30 +0900

    주석 추가

--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop2/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -18,14 +18,30 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    // 회원 생성 - 엔티티 사용
+    /**
+     * 회원 생성 - 요청 값으로 엔티티를 직접 받음.
+     *
+     * ==> 문제점 <==
+     * 엔티티에 화면을 위한 로직이 들어간다.(@NotEmpty 등)
+     * 엔티티가 변경되면 API 스펙이 변한다.
+     *
+     * ====> 결론 <=====
+     * API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     */
     @PostMapping("api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
-    // 회원 생성 - dto 사용
+    /**
+     * 회원 생성 - 요청 값으로 별도의 DTO를 받는다.
+     *
+     * == 장점 ==
+     * 엔티티와 프레젠테이션 계층을 위한 로직을 분리할 수 있다.
+     * 엔티티와 API 스펙을 명확하게 분리할 수 있다.
+     * 엔티티가 변해도 API 스펙이 변하지 않는다.
+     */
     @PostMapping("api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
@@ -35,7 +51,13 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
-    // 회원 수정
+    /**
+     * 회원 수정
+     * => 변경 감지 이용해서 데이터 수정
+     *
+     * == PUT은 전체 업데이트를 할 때 사용하는 것이 맞다.
+     *    부분 업데이트를 하려면 PATCH나 POST를 사용하자.
+     */
     @PutMapping("api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
                                              @RequestBody @Valid UpdateMemberRequest request) {
@@ -44,13 +66,25 @@ public class MemberApiController {
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
-    // 회원 조회 - 엔티티 외부에 직접 노출
-    // @JsonIgnore로 특정 값 노출을 막을수는 있지만
+    /**
+     * 회원 조회 - 응답 값으로 엔티티를 외부에 직접 노출.
+     *
+     * ==> 문제점 <==
+     * 응답 스펙에 맞추기 위해 @JsonIgnore 등 로직이 추가된다.
+     */
     @GetMapping("api/v1/members")
     public List<Member> membersV1() {
         return memberService.findMembers();
     }
 
+    /**
+     * 회원 조회 - 응답 값으로 별도의 DTO를 반환.
+     *
+     * == 추가 ==
+     * Result 클래스로 컬렉션을 감싸서 향후 필요한 필드를 추가 할 수 있음.
+     * collect => 배열
+     * new Result(collect) => 객체? 겉을 감쌈
+     */
     @GetMapping("api/v2/members")
     public Result membersV2() {
         List<Member> findMembers = memberService.findMembers();
