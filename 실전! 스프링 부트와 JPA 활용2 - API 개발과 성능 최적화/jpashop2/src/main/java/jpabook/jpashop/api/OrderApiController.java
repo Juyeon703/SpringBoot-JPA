1b4b58598fa6e9b05e08d346aa07f33ee1eb2c0e
Juyeon703 <sh6244@naver.com>
2023-05-21 01:11:46 +0900

    V3 +패치 조인 최적화

--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -45,6 +45,8 @@ public class OrderApiController {
      *
      * - Dto안에서도 엔티티 대신에 Dto 사용해서 반환.(OrderItemDto)
      * -> 껍데기만 노출하는 것이 아닌 속에 있는 내용도 DTO로 사용.
+     *
+     * ==> 지연로딩으로 1+n+n+n sql 실행됨.
      */
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2() {
@@ -91,5 +93,28 @@ public class OrderApiController {
             orderPrice = orderItem.getOrderPrice();
             count = orderItem.getCount();
         }
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경가능)
+     *
+     * join(일대다)하면 order가 orderItem 갯수만큼 중복이 일어나서 그 갯수만큼 출력됐음
+     * 만약 한 주문에 orderItem이 2개라면 order가 2번 출력됨.
+     * ==> distinct 사용시
+     *     db에 distinct 쿼리 날려주고 엔티티가 중복인 경우에 걸러서 컬렉션에 담아줌.
+     *     다만, 페이징이 불가능하다.(메모리에서 페이징함 -> 위험)
+     *     또한, 컬렉션 페치 조인은 1개만 사용할 수 있다. 일대 다(일대 다) 이렇게 들어가면 안됨.
+     *
+     * ==> SpringBoot 3.X 버전(Hibernate 6) 이상시 distinct없이 중복제거됨.
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
     }
 }
