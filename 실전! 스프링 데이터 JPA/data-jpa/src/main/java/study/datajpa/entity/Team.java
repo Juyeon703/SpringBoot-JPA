eed2dcec7cfc98de8c0570c39f8aca69d51c0ee5
Juyeon703 <sh6244@naver.com>
2023-05-21 22:01:07 +0900

    예제 도메인

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/entity/Team.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/entity/Team.java
@@ -21,4 +21,8 @@ public class Team {
 
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
 }
