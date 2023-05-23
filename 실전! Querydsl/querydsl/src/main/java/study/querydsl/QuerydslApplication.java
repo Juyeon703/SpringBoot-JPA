4cd3351ec53e1ccceb60314b1e0b69f49002d0e8
Juyeon703 <sh6244@naver.com>
2023-05-23 21:00:10 +0900

    순수 JPA Repository와 Querydsl

--- 실전! Querydsl/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ 실전! Querydsl/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,13 +1,26 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+	/**
+	 * 참고: 동시성 문제는 걱정하지 않아도 된다.
+	 * 		왜냐하면 여기서 스프링이 주입해주는 엔티티 매니저는 실제 동작 시점에 진짜 엔티티 매니저를 찾아주는 프록시용 가짜 엔티티 매니저이다.
+	 * 		이 가짜 엔티티 매니저는 실제 사용 시점에 트랜잭션 단위로 실제 엔티티 매니저(영속성 컨텍스트)를 할당해준다.
+	 */
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
---
+++ 실전! Querydsl/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,65 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    /*
+    public MemberJpaRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+    */
+
+    // JPAQueryFactory 빈 등록 후
+    // @RequiredArgsConstructor로 대신할 수 있음.
+    public MemberJpaRepository(EntityManager em, JPAQueryFactory queryFactory) {
+        this.em = em;
+        this.queryFactory = queryFactory;
+    }
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member findMember = em.find(Member.class, id);
+        return Optional.ofNullable(findMember);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(QMember.member).fetch();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(QMember.member)
+                .where(QMember.member.username.eq(username))
+                .fetch();
+    }
+}
