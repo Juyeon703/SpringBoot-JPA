89dff7d8f318e48bd61b8774a92ded7e2cd367ba
Juyeon703 <sh6244@naver.com>
2023-05-24 23:45:08 +0900

    JPA Hint & Lock - 성능 최적화

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,13 +1,12 @@
 package study.datajpa.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
@@ -154,4 +153,21 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      *    - PersistenceUnitUtil util = em.getEntityManagerFactory().getPersistenceUnitUtil();
      *      util.isLoaded(member.getTeam());
      */
+
+    /**
+     * JPA Hint : SQL 힌트가 아니라 JPA 구현체에게 제공하는 힌트
+     * -> 성능최적화를 위함(변경감지를 위해 필요한 비교할 객체 2개를 변경감지 안할거라고 가정하고 1개만 관리하는것?)
+     */
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username); // readOnly라서 변경감지가 안됨 -> update 안됨.
+
+    // forCounting : 반환 타입으로 Page 인터페이스를 적용하면 추가로 호출하는 페이징을 위한 count 쿼리도 쿼리 힌트 적용(기본값 true )
+    @QueryHints(value = { @QueryHint(name = "org.hibernate.readOnly", value = "true")}, forCounting = true)
+    Page<Member> findByUsername(String name, Pageable pageable);
+
+    /**
+     * Lock
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String name);
 }
